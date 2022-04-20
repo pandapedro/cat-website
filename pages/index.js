d7e621bd3e4c788a36b3d8160c13e0d1cc965b24
pandapedro <pandapedro001@gmail.com>
2022-04-20 16:12:03 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Home = () => {
+  const { t } = useTranslation("common");
+
   return (
     <>
       <Head>
@@ -8,18 +12,27 @@ const Home = () => {
       </Head>
       <div className="home">
         <h1>Cat</h1>
-        <p1>A simple and multifunctional discord bot.</p1>
+        <p1>{t("p1")}</p1>
         <div className="buttons">
           <button onClick={(d) => (d.view.document.location.href = "/add")}>
-            Add me
+            {t("b1")}
           </button>
           <button onClick={(d) => (d.view.document.location.href = "/help")}>
-            Support
+            {t("b2")}
           </button>
         </div>
       </div>
     </>
   );
 };
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
 
 export default Home;
