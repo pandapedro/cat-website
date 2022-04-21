0b4de95cc7f459a21850eb304dbe468c0d67d2fd
pandapedro <pandapedro001@gmail.com>
2022-04-21 11:06:11 -0300

    ok

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   i18n: {
     locales: ["en-US", "pt-BR"],
-    defaultLocale: "pt-BR",
-    localePath: path.resolve("./public/locales/"),
+    defaultLocale: "en-US",
   },
 };
