e3261b84938f4008626f2e13e576136ef978db2f
pandapedro <pandapedro001@gmail.com>
2022-04-21 15:34:01 -0300

    ok

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,35 @@
-const Footer = () => {
-  return (
-    <div className="footer">
-      <footer>
-        <p>
-          Powered by: <a href="https://nextjs.org/">next.js</a>,{" "}
-          <a href="https://code.visualstudio.com/">vsc</a> &{" "}
-          <a href="https://abal.moe/Eris/">eris</a>
-        </p>
-      </footer>
-    </div>
-  );
-};
+export async function getStaticProps() {
+  let user = await fetch(
+    `https://discord.com/api/v9/users/485141005796900867`,
+    {
+      headers: {
+        Authorization: `Bot ${process.env.TOKEN}`,
+      },
+    }
+  ).then(async (res) => await res.json());
 
-export default Footer;
+  return {
+    props: {
+      user: user,
+    },
+    revalidate: 360,
+  };
+}
+
+export default function Footer({ user }) {
+  console.log(user);
+  
+  return (
+    <>
+      <div className="footer">
+        <footer>
+          <p>
+            Powered by: <a href="https://nextjs.org/">next.js</a>,{" "}
+            <a href="https://code.visualstudio.com/">vsc</a> &{" "}
+            <a href="https://abal.moe/Eris/">eris</a>
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+}
