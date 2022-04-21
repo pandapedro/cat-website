0f184781efc10a9d3c4c32a17ff654fa7b413d60
pandapedro <pandapedro001@gmail.com>
2022-04-21 11:46:02 -0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/help",
-        destination: `https://discord.gg/${process.env.HELP}`,
-        permanent: true,
-      },
-      {
-        source: "/add",
-        destination: `https://discord.com/api/oauth2/authorize?client_id=${process.env.ID}&permissions=8&scope=bot%20applications.commands`,
-        permanent: true,
-      },
-    ];
+  i18n: {
+    defaultLocale: "en-US",
+    locales: ["en-US", "pt-BR"],
   },
+  redirects: () => [
+    {
+      source: "/help",
+      destination: `https://discord.gg/${process.env.HELP}`,
+      permanent: true,
+    },
+    {
+      source: "/add",
+      destination: `https://discord.com/api/oauth2/authorize?client_id=${process.env.ID}&permissions=8&scope=bot%20applications.commands`,
+      permanent: true,
+    },
+  ],
 };
