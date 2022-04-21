b7549abccaf708f4cc2a912a33f7182896b0a6fe
pandapedro <pandapedro001@gmail.com>
2022-04-21 11:53:53 -0300

    ok

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,20 @@
 import Head from "next/head";
+import Script from "next/script";
 
 const Nf = () => {
-    return (
-      <>
-        <Head>
-          <title>Cat - non-existent page</title>
-        </Head>
-        <div className="home">
-          <h1>This page does not exist</h1>
-          <p1>Please, go back to home page ;)</p1>
-          <div className="buttons">
-            <button onClick={(d) => (d.view.document.location.href = "/")}>
-              Home page
-            </button>
-          </div>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Cat - 404</title>
+      </Head>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        onLoad={() => {
+          window.location.href = "/";
+        }}
+      />
+    </>
+  );
 };
 
 export default Nf;
