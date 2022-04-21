3b377b4e2451ee565d8b283bf63ff7b40251a21f
pandapedro <pandapedro001@gmail.com>
2022-04-21 12:19:14 -0300

    ok

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,12 @@ const Footer = () => {
     <div className="footer">
       <footer>
         <p>
-          <a>Created by: Quinn#6155 (Id: 485141005796900867)</a>
+          Website powered by: <a href="https://nextjs.org/">next.js</a>
+        </p>
+        <p>
+          Bot powered by: <a href="https://nodejs.org">node.js</a>,{" "}
+          <a href="https://code.visualstudio.com/">visual studio code</a> &{" "}
+          <a href="https://abal.moe/Eris/">eris</a>
         </p>
       </footer>
     </div>
